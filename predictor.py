dd5c6a2f53e010d4edd7481b3e8d5207910187bd
Aditya Mohan <107522658+TheAM1729@users.noreply.github.com>
2024-10-27 08:47:41 -0400

    Finalized the predictor

--- predictor.py
+++ predictor.py
@@ -8,24 +8,20 @@ import numpy as np
 class Predictor:
     def __init__(self) -> None:
         self.model = keras.models.load_model('trainedModelV1.keras')
-        self.mapping = {"C":{np.int64(60)}, 
-                        "A#":{np.int64(58)},
-                        "D":{np.int64(62)},
-                        "G":{np.int64(67)},
-                        "D#":{np.int64(64), np.int64(63)},
-                        "G#":{np.int64(68)},
-                        "C#":{np.int64(61)},
-                        "A":{np.int64(50), np.int64(69)},
-                        "F#":{np.int64(66)}
-                    }
+        self.mapping = {"50" : "a", "58" : "as", "59" : "b", "60" : "c",
+                        "61" : "cs", "62": "d", "63" : "ds", "64" : "e",
+                        "65" : "f", "66" : "fs", "67" : "g", "68" : "gs"}
 
     def getNote(self, spectrogramImagePath):
         i = []
         i.append(image.img_to_array(image.load_img(spectrogramImagePath, target_size=(372, 147, 3))))
         x_test = np.array(i) / 255
         prediction = self.model(x_test)
-        return self.mapping[np.argmax(prediction[0]).item()]
-    
+        try:
+            return self.mapping[np.argmax(prediction[0]).item()]
+        except KeyError:
+            return ""
+        
     def getNotes(self, spectrogramImagePaths):
         i = []
         for path in spectrogramImagePaths:
@@ -34,7 +30,10 @@ class Predictor:
         predictions = self.model(x_test)
         pred = []
         for i in predictions:
-            pred.append(i.value())
+            try:
+                pred.append(self.mapping[str(np.argmax(i).item())])
+            except KeyError:
+                pred.append("")
         return pred
     
     
